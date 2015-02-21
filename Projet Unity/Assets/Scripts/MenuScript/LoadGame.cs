f23e769b1ce52f3402c94846a98567068a99c078
Spirit <theo.couvert@yahoo.fr>
2015-02-21 19:19:53 +0100

    Udpate du menu
    On peut désormais quitter le jeu via le bouton Quitter et lancer une
    partie après le choix du personnage

---
+++ Projet Unity/Assets/Scripts/MenuScript/LoadGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour {
+
+	[SerializeField]
+	string _character;
+
+	void FixedUpdate()
+	{
+		Application.LoadLevel ("Gameplay");
+		print(_character);
+	}
+
+}
---
+++ Projet Unity/Assets/Scripts/MenuScript/QuitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	void FixedUpdate()
+	{
+		Application.Quit();
+	}
+
+}
