359bf8a3fcc813a470c752c5700b6b8915dd2a53
Spirit <theo.couvert@yahoo.fr>
2015-02-21 23:47:03 +0100

    Gui Serveur
    Ajout du menu affiché lors du choix du serveur pour les modes en
    multijoueur

---
+++ Projet Unity/Assets/Scripts/MenuScript/ServerScript.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------
+// Project: Darkness Defender
+// Script: LoadGame.cs
+// Author: Théo Couvert 3A 3DJV
+// --------------------------------------------------
+
+// Library
+using UnityEngine;
+using System.Collections;
+
+public class ServerScript : MonoBehaviour
+{
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+	
+	private bool isRefreshingHostList = false;
+	private HostData[] hostList;
+
+	[SerializeField]
+	GameObject _parent;
+	
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(70, 100, 150, 80), "Démarrer un serveur"))
+				print ("Start");
+			
+			if (GUI.Button(new Rect(70, 200, 150, 80), "Rafraichir"))
+				print ("Refresh");
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						print ("Connexion");
+				}
+			}
+			if (GUI.Button(new Rect(500, 500, 120, 50), "Retour"))
+			{
+				this.gameObject.SetActive(false);
+				_parent.SetActive(true);
+			}
+		}
+	}
+}
