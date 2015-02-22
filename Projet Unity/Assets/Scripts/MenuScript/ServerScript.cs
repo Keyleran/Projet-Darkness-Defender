794aa25717097ecaaf89b71e50de73bca82c0248
Spirit <theo.couvert@yahoo.fr>
2015-02-22 01:30:55 +0100

    Retour du réseau
    Remise en marche du réseau avec une version qui reste basique (un nom
    unique de serveur et de partie pour le moment et pas la possibilité de
    le changer). La version détecte le type de partie jouée et l'affiche

--- Projet Unity/Assets/Scripts/MenuScript/ServerScript.cs
+++ Projet Unity/Assets/Scripts/MenuScript/ServerScript.cs
@@ -10,31 +10,37 @@ using System.Collections;
 
 public class ServerScript : MonoBehaviour
 {
-	private const string typeName = "UniqueGameName";
-	private const string gameName = "RoomName";
+	private const string typeName = "Partie de test";
+	private const string gameName = "Room de Test";
+
+	private string gameType;
+	private string difficulty = null;
 	
 	private bool isRefreshingHostList = false;
 	private HostData[] hostList;
 
 	[SerializeField]
 	GameObject _parent;
+
+	[SerializeField]
+	GameObject _destination;
 	
 	void OnGUI()
 	{
 		if (!Network.isClient && !Network.isServer)
 		{
 			if (GUI.Button(new Rect(70, 100, 150, 80), "Démarrer un serveur"))
-				print ("Start");
+				StartServer();
 			
 			if (GUI.Button(new Rect(70, 200, 150, 80), "Rafraichir"))
-				print ("Refresh");
+				RefreshHostList();
 			
 			if (hostList != null)
 			{
 				for (int i = 0; i < hostList.Length; i++)
 				{
-					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
-						print ("Connexion");
+					if (GUI.Button(new Rect(250, 100 + (110 * i), 600, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
 				}
 			}
 			if (GUI.Button(new Rect(500, 500, 120, 50), "Retour"))
@@ -43,5 +49,54 @@ public class ServerScript : MonoBehaviour
 				_parent.SetActive(true);
 			}
 		}
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer(2, 6500, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName, gameType + difficulty);
+		_destination.gameObject.SetActive (true);
+	}
+	
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+		//SpawnPlayer();
+	}
+	
+	
+	void Update()
+	{
+		if (isRefreshingHostList && MasterServer.PollHostList().Length > 0)
+		{
+			isRefreshingHostList = false;
+			hostList = MasterServer.PollHostList();
+		}
+	}
+	
+	private void RefreshHostList()
+	{
+		if (!isRefreshingHostList)
+		{
+			isRefreshingHostList = true;
+			MasterServer.RequestHostList(typeName);
+		}
+	}
+
+	public void TypeGame(string gameMode)
+	{
+		gameType = gameMode;
+		Debug.Log(gameType);
+	}
+
+	public void Difficulty(string chosenDifficulty)
+	{
+		difficulty = chosenDifficulty;
+		Debug.Log(gameType);
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
 	}
 }
